c56fe341db4805cd2f89933be25df2b6f5b91644
Cedric Ziel <cedric@cedric-ziel.com>
2026-06-11 20:48:53 +0200

    style: drop redundant Sendable conformance on Campaign/AdGroup
    Internal structs with all-Sendable members infer Sendable automatically.

--- Sponsorly/Features/Campaigns/Campaign.swift
+++ Sponsorly/Features/Campaigns/Campaign.swift
@@ -1,29 +1,33 @@
 import Foundation
 
 /// A Sponsored Products campaign (subset of the SP v3 management fields).
-struct Campaign: Identifiable, Sendable, Decodable, Hashable {
+struct Campaign: Identifiable, Decodable, Hashable {
     let campaignId: String
     let name: String
     let state: String
     let targetingType: String?
     let budget: Budget?
 
-    var id: String { campaignId }
+    var id: String {
+        campaignId
+    }
 
-    struct Budget: Sendable, Decodable, Hashable {
+    struct Budget: Decodable, Hashable {
         let budget: Double?
         let budgetType: String?
     }
 }
 
 /// A Sponsored Products ad group (subset of the SP v3 fields).
-struct AdGroup: Identifiable, Sendable, Decodable, Hashable {
+struct AdGroup: Identifiable, Decodable, Hashable {
     let adGroupId: String
     let name: String
     let state: String
     let defaultBid: Double?
 
-    var id: String { adGroupId }
+    var id: String {
+        adGroupId
+    }
 }
 
 /// SP v3 `POST /sp/campaigns/list` response envelope.
